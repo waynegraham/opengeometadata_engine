89a599737d61cb1dded787d6a7163b110a218646
Wayne Graham <wayne.graham@virginia.edu>
2015-06-11 10:14:33 -0400

    Add basic app

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 OpengeometadataEngine::Engine.routes.draw do
+  get 'metadata/:institution.:format' => 'opengeometadata#index',
+    :constraints => { :institution => /[\w\.]+/ },
+    :defaults => { :format => 'json' }
+
+  get 'metadata/:institution/:layer_id/:metadata_format' => 'opengeometadata#show',
+    :constraints => { :institution => /[\w\.]+/, :layer_id => /[\w\.:\/]+/ },
+    :defaults => { :format => 'xml' }
 end
--- lib/opengeometadata_engine/engine.rb
+++ lib/opengeometadata_engine/engine.rb
@@ -1,5 +1,8 @@
 module OpengeometadataEngine
   class Engine < ::Rails::Engine
     isolate_namespace OpengeometadataEngine
+
+    config.datadir = '/var/cache/opengeometadata'
+
   end
 end
